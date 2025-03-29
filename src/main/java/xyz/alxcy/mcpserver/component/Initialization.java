5ed6735f9c8221ad1376505dced238760912c228
柳笑辰 <liuxiaochen@liuxiaochendeMacBook-Pro.local>
2025-03-29 16:46:09 +0800

    Initial commit

---
+++ src/main/java/xyz/alxcy/mcpserver/component/Initialization.java
@@ -0,0 +1,15 @@
+package xyz.alxcy.mcpserver.component;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Initialization implements InitializingBean {
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
